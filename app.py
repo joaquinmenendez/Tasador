0e5d764fdd7b1acf49cfe964f267a2ecbcf54e74
joaquin.menendez <joaquin.menendez@globant.com>
2020-11-30 13:49:01 -0300

    Clase y Falsk app

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, url_for, request, flash, redirect
+import os
+from tasador_app import Property
+
+#Initialize variables
+app = Flask(__name__, static_url_path="/tmp", static_folder="tmp")
+app.secret_key = "" #Flask ask me for a key.
+
+#Funcion predictora
+def prediccion(propiedad):
+    return [propiedad.rooms,
+            propiedad.bedrooms,
+            propiedad.bathrooms,
+            propiedad.surface_total,
+            propiedad.surface_covered,
+            round(propiedad.predictValue())
+            ]
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+# --- Tasador
+
+@app.route('/tasar')
+def tasador_page():
+    return render_template('tasar.html')
+
+
+@app.route('/tasar', methods=['POST'])
+def tasar_prop():
+    try:
+        rooms = int(request.form['rooms'])
+        bedrooms = int(request.form['bedrooms'])
+        bathrooms = int(request.form['bathrooms'])
+        surface_total = int(request.form['sup_total'])
+        surface_covered = int(request.form['sup_cub'])
+        propiedad = Property(rooms, bedrooms, bathrooms, surface_total, surface_covered)
+    except Exception as e:
+        print(e)
+        return render_template('/error.html')  # No queria lidiar con errores ni nada fancy
+    print(propiedad)
+    print(round(propiedad.predictValue()))
+    prediction = prediccion(propiedad)
+    return render_template('/prediction.html', prediction=prediction)
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+# Run main
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ tasador_app.py
@@ -0,0 +1,43 @@
+import pickle
+import numpy as np
+
+# Load ML model and Scaler
+with open('tmp/model.pickle', 'rb') as file:
+    model = pickle.load(file)
+
+
+# Define property
+class Property:
+
+    def __init__(self,rooms, bedrooms, bathrooms, surface_total, surface_covered):
+        self.rooms = rooms
+        self.bedrooms = bedrooms
+        self.bathrooms = bathrooms
+        self.surface_total = surface_total
+        self.surface_covered = surface_covered
+
+    def __repr__(self):
+        return (f'{self.__class__.__name__}('
+                f'{self.rooms}, {self.bedrooms}, {self.bathrooms}, '
+                f'{self.surface_total}, {self.surface_covered})')
+
+    def predictValue(self, printable=False):
+        """
+        Returns an estimate price in dollars for the property
+        :param printable: Bool - Default: False. Prints a message with the estimated value
+        :return: float
+        """
+        # Create an array with the object attributes
+        property_values = [att for att in self.__dict__.values()]
+        property_values = np.array(property_values).reshape(1, -1)
+        property_values = model["scaler"].transform(property_values)
+        price_pred = model["modelo"].predict(property_values)[0]
+        if printable:
+            print(f'Esta propiedad tiene un valor estimado de: {price_pred:.0f}')
+        else:
+            return price_pred
+
+
+if __name__ == "__main__":
+    nueva_prop = Property()
+    nueva_prop.predictValue(printable=True)
